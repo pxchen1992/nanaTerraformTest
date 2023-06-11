eb90d5352cc8203e7ed5b6d2b6749fd9d063325b
pengxiang chen <pengxiangchen1992@hotmail.com>
2023-06-11 13:48:55 +1000

    add more

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = ap-southeast-2
+    region = "ap-southeast-2"
 }
 
 #resource "<provider>_<resourceType>" "name we can define" {
@@ -11,8 +11,8 @@ variable avali_zone {}
 variable env_prefix {}
 
 resource "aws_vpc" "myapp-vpc" {
-    cidr_block = var.vpc_cidr_blocks
-    tag = {
+    cidr_block = var.vpc_cidr_block
+    tags = {
         Name: "${var.env_prefix}-vpc"
     }
 }
@@ -25,3 +25,38 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name: "${var.env_prefix}-subnet-1"
   }
 }
+
+
+# resource "aws_route_table" "myapp-route-table" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-igw.id
+#   }
+#   tags = {
+#     Name: "${var.env_prefix}-rtb"
+#   }
+# }
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-rtb"
+  }
+}
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
