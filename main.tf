fa534453ea4d15f32b56f5cadc94d01f75d0fa71
pengxiang chen <pengxiangchen1992@hotmail.com>
2023-06-15 23:09:37 +1000

    fix/user_data

--- main.tf
+++ main.tf
@@ -134,21 +134,22 @@ resource "aws_instance" "myapp-server" {
 
   key_name = aws_key_pair.nana-ssh-key.key_name
 
-  # user_data = file("entry-script.sh")
+  user_data = file("entry-script.sh")
 
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ec2-user"
-    private_key = file(var.private_key_location)
-  }
-  provisioner "remote-exec" {
-    inline = [ 
-      "export ENV-dev",
-      "mkdir newdir2"
-     ]
-    
-  }
+  # connection {
+  #   type = "ssh"
+  #   host = self.public_ip
+  #   user = "ec2-user"
+  #   private_key = file(var.private_key_location)
+  # }
+  # provisioner "file" {
+  #   source = "entry-script.sh"
+  #   destination = "/home/ec2-user/entry-script.sh"
+  
+  # }
+  # provisioner "remote-exec" {
+  #   script = file("entry-script.sh")
+  # }
 
   tags = {
     Name = "${var.env_prefix}-server"
